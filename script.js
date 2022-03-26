a5fbcb3fc802ae5e3376f831020c63476cc98cae
Tariq Khan <71902492+tariq-k-dev@users.noreply.github.com>
2022-03-26 15:33:29 -0700

    Add spinning loader

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
+const container = document.querySelector('.container');
+const loaderContainer = document.querySelector('.loader-container');
+const tweetBtn = document.querySelector('#twitter');
+
+// Show loading
+function loading() {
+  loaderContainer.style.display = 'flex';
+  container.style.display = 'none';
+}
+
+// Hide loading
+function complete() {
+  loaderContainer.style.display = 'none';
+  container.style.display = 'flex';
+}
+
 async function getQuotes() {
-  try {
-    const response = await fetch('https://type.fit/api/quotes')
-    quotes = await response.json()
-    return quotes
-  } catch (err) {
-    console.error(err)
+  const localQuotes = localStorage.getItem('quotes');
+
+  if (localQuotes !== null) {
+    return JSON.parse(localQuotes);
+  } else {
+    try {
+      const response = await fetch('https://type.fit/api/quotes');
+      quotes = await response.json();
+
+      return quotes;
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
 async function storeQuotes() {
-  const quotes = await getQuotes()
-  localStorage.setItem('quotes', JSON.stringify(quotes))
-  getQuote()
+  loading();
+  const quotes = await getQuotes();
+  localStorage.setItem('quotes', JSON.stringify(quotes));
+  getQuote();
 }
 
 function getQuote() {
-  const storedQuotesStr = localStorage.getItem('quotes')
+  const storedQuotesStr = localStorage.getItem('quotes');
 
   if (storedQuotesStr) {
-    const storedQuotes = JSON.parse(storedQuotesStr)
-    const index = Math.round(Math.random() * storedQuotes.length)
-    const { text, author } = storedQuotes[index]
-    const quoteElem = document.getElementById('quote')
-    const authorElem = document.getElementById('author')
+    const storedQuotes = JSON.parse(storedQuotesStr);
+    const index = Math.round(Math.random() * storedQuotes.length);
+    const { text, author } = storedQuotes[index];
+    const quoteElem = document.getElementById('quote');
+    const authorElem = document.getElementById('author');
     // Replace null author value with 'Unknown'
-    const authorFixed = author ? author : 'Unknown'
+    const authorFixed = author ? author : 'Unknown';
 
     localStorage.setItem(
       'quoteData',
@@ -32,26 +56,27 @@ function getQuote() {
         quote: text,
         author: authorFixed,
       })
-    )
-    quoteElem.innerText = text
-    authorElem.innerText = `- ${authorFixed}`
+    );
+    quoteElem.innerText = text;
+    authorElem.innerText = `-${authorFixed}`;
+
+    complete();
   }
 }
 
 function tweetQuote(e) {
-  e.preventDefault()
+  e.preventDefault();
 
-  const quoteData = JSON.parse(localStorage.getItem('quoteData'))
-  const quote = encodeURIComponent(quoteData.quote)
-  const author = encodeURIComponent(quoteData.author)
-  const url = encodeURIComponent(window.location.href)
-  const text = `${quote}  ${author}  ${url}`
-  const tweetableQuote = `https://twitter.com/intent/tweet?text=${text}`
+  const quoteData = JSON.parse(localStorage.getItem('quoteData'));
+  const quote = encodeURIComponent(quoteData.quote);
+  const author = encodeURIComponent(quoteData.author);
+  const url = encodeURIComponent(window.location.href);
+  const text = `"${quote}"  - ${author}   ${url}`;
+  const tweetableQuote = `https://twitter.com/intent/tweet?text=${text}`;
 
-  window.open(tweetableQuote, 'target="_blank"')
+  window.open(tweetableQuote, 'target="_blank"');
 }
 
-const tweetBtn = document.querySelector('#twitter')
-tweetBtn.addEventListener('click', tweetQuote)
+tweetBtn.addEventListener('click', tweetQuote);
 
-storeQuotes()
+storeQuotes();
